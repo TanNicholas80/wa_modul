77bb97f7df5c3fa7e245e092cb4af8149882160a
luthfi9251 <luthfi.irfan11@gmail.com>
2024-02-02 13:07:57 +0700

    client promises

--- client.js
+++ client.js
@@ -1,58 +1,109 @@
-const { Client, RemoteAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const { Client, RemoteAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
 
-const { MongoStore } = require('wwebjs-mongo');
-const mongoose = require('mongoose');
+const { MongoStore } = require("wwebjs-mongo");
+const mongoose = require("mongoose");
 
-const MONGODB_URI = "mongodb+srv://nicholas:Tannicholas876@whatsappcluster.rt5xbqr.mongodb.net/whatsapp_otp";
+const MONGODB_URI =
+    "mongodb+srv://nicholas:Tannicholas876@whatsappcluster.rt5xbqr.mongodb.net/whatsapp_otp";
 
-const initializeClient = async () => {
-    await mongoose.connect(MONGODB_URI);
-    console.log("MongoDB Connected");
-    const store = new MongoStore({ mongoose: mongoose });
+const initializeClient = new Promise((resolve, reject) => {
+    mongoose.connect(MONGODB_URI).then(() => {
+        console.log("MongoDB Connected");
+        const store = new MongoStore({ mongoose: mongoose });
 
-    const client = new Client({
-        authStrategy: new RemoteAuth({
-            clientId: 'whatsapp_user_remote',
-            dataPath: './.wwebjs_auth',
-            store: store,
-            backupSyncIntervalMs: 300000,
-        }),
-        puppeteer: { 
-            headless: true
-        },
-    });
+        const client = new Client({
+            authStrategy: new RemoteAuth({
+                clientId: "whatsapp_user_remote",
+                dataPath: "./.wwebjs_auth",
+                store: store,
+                backupSyncIntervalMs: 300000,
+            }),
+            puppeteer: {
+                headless: true,
+            },
+        });
 
-    client.initialize();
+        client.initialize();
 
-    client.on('remote_session_saved', () => {
-        console.log('remote_session_saved');
-    });
-    
-    client.on('loading_screen', (percent, message) => {
-        console.log('LOADING SCREEN', percent, message);
-    });
-    
-    client.on('qr', (qr) => {
-        // NOTE: This event will not be fired if a session is specified.
-        console.log('QR RECEIVED', qr);
-        qrcode.generate(qr, { small: true });
-    });
-    
-    client.on('authenticated', () => {
-        console.log('AUTHENTICATED');
-    });
-    
-    client.on('auth_failure', msg => {
-        // Fired if session restore was unsuccessful
-        console.error('AUTHENTICATION FAILURE', msg);
-    });
-    
-    client.on('ready', () => {
-        console.log('READY');
-    });
+        client.on("remote_session_saved", () => {
+            console.log("remote_session_saved");
+        });
 
-    return client;
-};
+        client.on("loading_screen", (percent, message) => {
+            console.log("LOADING SCREEN", percent, message);
+        });
+
+        client.on("qr", (qr) => {
+            // NOTE: This event will not be fired if a session is specified.
+            console.log("QR RECEIVED", qr);
+            qrcode.generate(qr, { small: true });
+        });
+
+        client.on("authenticated", () => {
+            console.log("AUTHENTICATED");
+        });
+
+        client.on("auth_failure", (msg) => {
+            // Fired if session restore was unsuccessful
+            console.error("AUTHENTICATION FAILURE", msg);
+            reject("AUTHENTICATION FAILURE", msg);
+        });
+
+        client.on("ready", () => {
+            console.log("READY");
+            resolve(client);
+        });
+    });
+});
+
+// const initializeClient1 = async () => {
+//     await mongoose.connect(MONGODB_URI);
+//     console.log("MongoDB Connected");
+//     const store = new MongoStore({ mongoose: mongoose });
+
+//     const client = new Client({
+//         authStrategy: new RemoteAuth({
+//             clientId: 'whatsapp_user_remote',
+//             dataPath: './.wwebjs_auth',
+//             store: store,
+//             backupSyncIntervalMs: 300000,
+//         }),
+//         puppeteer: {
+//             headless: true
+//         },
+//     });
+
+//     client.initialize();
+
+//     client.on('remote_session_saved', () => {
+//         console.log('remote_session_saved');
+//     });
+
+//     client.on('loading_screen', (percent, message) => {
+//         console.log('LOADING SCREEN', percent, message);
+//     });
+
+//     client.on('qr', (qr) => {
+//         // NOTE: This event will not be fired if a session is specified.
+//         console.log('QR RECEIVED', qr);
+//         qrcode.generate(qr, { small: true });
+//     });
+
+//     client.on('authenticated', () => {
+//         console.log('AUTHENTICATED');
+//     });
+
+//     client.on('auth_failure', msg => {
+//         // Fired if session restore was unsuccessful
+//         console.error('AUTHENTICATION FAILURE', msg);
+//     });
+
+//     client.on('ready', () => {
+//         console.log('READY');
+//     });
+
+//     return client;
+// };
 
 module.exports = { mongoose, initializeClient };
